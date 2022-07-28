3f317483d1cc09a13ffa516d69302a0c88d17f90
river <rrivverr@protonmail.com>
2022-07-28 01:22:47 -0400

    Begin work on layout and design

--- src/index.js
+++ src/index.js
@@ -4,31 +4,18 @@ import { Project } from './project';
 import { addProjectButton, createListeners, renderProject, renderTask } from './interface';
 
 
-const destDate = ("7/25/2022");
 
-const test = Task("New ggTitle", destDate, 3, "Test Description");
-const test2 = Task("Second Task", destDate, 1, "Nothin rly")
-//const restProject = Project("PROJECTO", destDate, 2, "Project Scription")
-const testProject = Project("Project Title", destDate, 2, "PROJECT DESCRIPTION")
-
-const altProject = Project();
-
-
-
-altProject.tasks.addTask(test)
-altProject.tasks.addTask(test2)
-
-testProject.tasks.addTask(test)
-
-
-const content = document.querySelector("#content");
 
 
 //const testProj = renderProject(testProject);
 
 
+
+
+
+
 const NEWPROJ = Project();
-const newTask = Task();
+
 
 
 
